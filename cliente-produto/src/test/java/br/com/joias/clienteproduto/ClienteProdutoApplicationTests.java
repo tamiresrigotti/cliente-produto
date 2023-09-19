80725f9231acc55af6a692a64b47b20aeff9fddd
tamiresrigoti <tamiresrigoti@gmail.com>
2023-09-19 20:18:05 -0300

    inicia o projeto

---
+++ cliente-produto/src/test/java/br/com/joias/clienteproduto/ClienteProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joias.clienteproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/br/com/joias/clienteproduto/ClienteProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joias.clienteproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteProdutoApplication.class, args);
+	}
+
+}
