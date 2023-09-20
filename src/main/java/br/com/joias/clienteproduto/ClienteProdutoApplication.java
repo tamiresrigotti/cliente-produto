e815ea01cbdf7fbd4fbc578282ec3dfc5fb9bb7b
tamiresrigoti <tamiresrigoti@gmail.com>
2023-09-20 17:44:51 -0300

    implementa api de teste

--- src/main/java/br/com/joias/clienteproduto/ClienteProdutoApplication.java
+++ src/main/java/br/com/joias/clienteproduto/ClienteProdutoApplication.java
@@ -2,10 +2,21 @@ package br.com.joias.clienteproduto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
+
 public class ClienteProdutoApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente Produto - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteProdutoApplication.class, args);
 	}
