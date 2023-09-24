5c89db81fb86cd1f48336c258fa412d33c7537c9
tamiresrigoti <tamiresrigoti@gmail.com>
2023-09-24 17:28:04 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/joias/clienteproduto/cliente/domain/Cliente.java
+++ src/main/java/br/com/joias/clienteproduto/cliente/domain/Cliente.java
@@ -4,12 +4,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.id.CompositeNestedGeneratedValueGenerator.GenerationContextLocator;
 import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.AccessLevel;
@@ -19,9 +24,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Entity
-
 public class Cliente {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id" , updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
 	@NotBlank
 	private String nomeCompleto;
@@ -44,7 +50,6 @@ public class Cliente {
 	public Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, String celular, String telefone,
 			Sexo sexo, @NotNull LocalDate dataNascimento, @CPF String cpf, Boolean aceitaTermos,
 			LocalDateTime momentoDoCadastro) {
-		this.idCliente = UUID.randomUUID();
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
 		this.celular = celular;
